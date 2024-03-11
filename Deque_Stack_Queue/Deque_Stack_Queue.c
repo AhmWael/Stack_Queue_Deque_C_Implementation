fda5a58bb72ede293bc84403b85cfd1748383b82
Ahmad Wael <ahmadwael2004@gmail.com>
2024-03-12 01:54:17 +0200

    Added deque, stack, array implementation using linked lists
    - Used singly grounded linked list

---
+++ Deque_Stack_Queue/Deque_Stack_Queue.c
@@ -0,0 +1,182 @@
+#include "deque_stack_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+node *new_node(TYPE data)
+{
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    n->data = data;
+    n->next = NULL;
+    return n;
+}
+
+deque *new_deque()
+{
+    deque *d = malloc(sizeof(deque));
+    if (d == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    d->head = d->tail = NULL;
+    return d;
+}
+
+void distNode(node *n)
+{
+    free(n);
+}
+
+void insHead(deque *d, TYPE data)
+{
+    node *n = new_node(data);
+    if (!d->head)
+        d->head = d->tail = n;
+    else
+    {
+        n->next = d->head;
+        d->head = n;
+    }
+}
+
+void remHead(deque *d)
+{
+    if (d->head)
+    {
+        if (d->head == d->tail)
+        {
+            d->head = d->tail = NULL;
+        }
+        else
+        {
+            node *temp = d->head;
+            d->head = d->head->next;
+            distNode(temp);
+        }
+    }
+}
+
+void insTail(deque *d, TYPE data)
+{
+    node *n = new_node(data);
+    if (!d->head)
+        d->head = d->tail = n;
+    else
+    {
+        d->tail->next = n;
+        d->tail = n;
+    }
+}
+
+void remTail(deque *d)
+{
+    if (d->tail)
+    {
+        if (d->head == d->tail)
+        {
+            d->head = d->tail = NULL;
+        }
+        else
+        {
+            node *temp = d->head;
+            while (temp->next != d->tail)
+            {
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            distNode(d->tail);
+            d->tail = temp;
+        }
+    }
+}
+
+void printDeque(deque *d)
+{
+    node *current = d->head;
+    while (current != NULL)
+    {
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+}
+
+/************** Stack Functions **************/
+stack *new_stack()
+{
+    stack *s = malloc(sizeof(stack));
+    if (s == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->deque = new_deque();
+    return s;
+}
+
+void push_stack(stack *s, TYPE data)
+{
+    insHead(s->deque, data);
+}
+
+TYPE pop_stack(stack *s)
+{
+    if (!isEmptyStack(s))
+    {
+        TYPE data = s->deque->head->data;
+        remHead(s->deque);
+        return data;
+    }
+    else
+        return -1;
+}
+
+TYPE peek_stack(stack *s)
+{
+    return !isEmptyStack(s) ? s->deque->head->data : -1;
+}
+
+int isEmptyStack(stack *s)
+{
+    return s->deque->head == NULL ? 1 : 0;
+}
+/*********************************************/
+/************** Queue Functions **************/
+queue *new_queue()
+{
+    queue *q = malloc(sizeof(queue));
+    if (q == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    q->deque = new_deque();
+    return q;
+}
+
+void enqueue(queue *q, TYPE data)
+{
+    insTail(q->deque, data);
+}
+
+TYPE dequeue(queue *q)
+{
+    if (!isEmptyQueue(q))
+    {
+        TYPE data = q->deque->head->data;
+        remHead(q->deque);
+        return data;
+    }
+    else
+        return -1;
+}
+
+int isEmptyQueue(queue *q)
+{
+    return q->deque->head == NULL ? 1 : 0;
+}
+/*********************************************/
---
+++ Deque_Stack_Queue/deque_stack_queue.h
@@ -0,0 +1,54 @@
+#ifndef DEQUE_STACK_QUEUE_H
+#define DEQUE_STACK_QUEUE_H
+
+typedef int TYPE;
+
+typedef struct
+{
+    TYPE data;
+    struct node *next;
+} node;
+
+typedef struct
+{
+    node *head;
+    node *tail;
+} deque;
+
+typedef struct
+{
+    deque *deque;
+} stack;
+
+typedef struct
+{
+    deque *deque;
+} queue;
+
+// Primary functions for deque
+node *new_node(TYPE data);
+deque *new_deque();
+void distNode(node *n);
+void insHead(deque *d, TYPE data);
+void remHead(deque *d);
+void insTail(deque *d, TYPE data);
+void remTail(deque *d);
+void printDeque(deque *d);
+
+// Stack functions
+stack *new_stack();
+// void dist_stack(stack *s);
+void push_stack(stack *s, TYPE data);
+TYPE pop_stack(stack *s);
+TYPE peek_stack(stack *s);
+int isEmptyStack(stack *s);
+void print_Stack(stack *s);
+
+// Queue functions
+queue *new_queue();
+// void dist_queue(queue *q);
+void enqueue(queue *q, TYPE data);
+TYPE dequeue(queue *q);
+int isEmptyQueue(queue *q);
+
+#endif /* DEQUE_STACK_QUEUE_H */
